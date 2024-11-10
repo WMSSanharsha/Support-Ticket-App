4d6b10d029a1431bc08bc4897fb81c956aca8791
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-11-10 20:52:29 +0530

    create signIn login and home pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -17,6 +19,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
+      <ToastContainer />
     </>
   );
 }
