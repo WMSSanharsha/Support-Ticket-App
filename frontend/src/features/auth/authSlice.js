08103931e9b32bddcb6e179eacfa168ddd624e19
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-11-11 11:54:21 +0530

    add login and signup state

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -8,6 +8,20 @@ const initialState = {
   message: "",
 };
 
+// Register New User
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    console.log(user);
+  }
+);
+
+// Login New User
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  console.log(user);
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
